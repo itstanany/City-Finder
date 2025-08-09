a5af93c7077c95613427264f4430da884920e1fc
Tanany <itstanany@gmail.com>
2025-08-09 14:54:59 +0300

    domain-layer/implement get all cities use case

---
+++ app/src/main/java/com/itstanany/cityfinder/domain/usecase/GetAllCitiesUseCase.kt
@@ -0,0 +1,22 @@
+package com.itstanany.cityfinder.domain.usecase
+
+import com.itstanany.cityfinder.domain.model.City
+import com.itstanany.cityfinder.domain.repository.CityRepository
+import jakarta.inject.Inject
+
+class GetAllCitiesUseCase @Inject constructor(
+  private val cityRepository: CityRepository
+) {
+  suspend operator fun invoke(): List<City> {
+    return cityRepository.getAllCities()
+    // IMPORTANT NOTE:
+    // enhancement if we don't have a guarantee that the result is sorted by name from the source
+    // in this project, we pre-processed the data to be sorted alphabetically by city name
+    /*
+      return repository.getAllCities()
+                  .sortedWith(compareBy<City> { it.name.lowercase() }
+                      .thenBy { it.countryCode.lowercase() })
+     */
+  }
+
+}
