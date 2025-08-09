d00e7fe52bcb03c4a43eec1ffd7348c0b857a120
Tanany <itstanany@gmail.com>
2025-08-09 18:20:19 +0300

    data-layer/implement local data

---
+++ app/src/main/java/com/itstanany/cityfinder/data/model/CoordResponse.kt
@@ -0,0 +1,12 @@
+package com.itstanany.cityfinder.data.model
+
+/**
+ * Data class representing the geographical coordinates (longitude and latitude)
+ *
+ * @property lon The longitude coordinate.
+ * @property lat The latitude coordinate.
+ */
+data class CoordResponse(
+  val lon: Double,
+  val lat: Double
+)
